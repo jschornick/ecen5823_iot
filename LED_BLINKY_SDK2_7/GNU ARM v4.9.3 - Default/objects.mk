725165a7626c3a6340e8159df004979f16144b00
Jeff Schornick <jeff@schornick.org>
2018-01-28 19:19:42 -0700

    Import Blinky project

---
+++ LED_BLINKY_SDK2_7/GNU ARM v4.9.3 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/jeff/git/LED_BLINKY_SDK2_7/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/libmbedtls.a /home/jeff/git/LED_BLINKY_SDK2_7/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg1_gcc_release.a /home/jeff/git/LED_BLINKY_SDK2_7/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/libpsstore.a /home/jeff/git/LED_BLINKY_SDK2_7/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/libbluetooth.a /home/jeff/git/LED_BLINKY_SDK2_7/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/binapploader.o
+
+LIBS := -lm
+
